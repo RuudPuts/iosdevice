ada36e717e0b5575c840cc721db1bd3369356557
Ruud Puts <ruud.puts@philips.com>
2014-12-04 22:27:31 +0100

    Basic parameter parser

--- iosdevice.m
+++ iosdevice.m
@@ -1,6 +1,39 @@
 #include "iosdevice.h"
 
-int main(int argc, char *argv[])
-{
-	printf("%s\n", "iOSDevice here");
+const char *device_udid;
+
+static void print_help() {
+	printf("iosdevice usage:\niosdevice [-u]\n\nParameters:\n\n\tu\tUDID of a specific device to filter on\n\n");
+}
+
+int main (int argc, char *argv[]) {
+	if (argc == 1) {
+		print_help();
+		return 0;
+	}
+
+	int index;
+	for(index = 0; index < argc; index++) {
+		printf("The %d is %s\n", index,argv[index]);
+	}
+
+	int opt;
+
+	while ((opt = getopt(argc, argv, "u:h")) != -1) {
+		printf("opt\n");
+		switch (opt) {
+			case 'u':
+				device_udid = optarg;
+				printf("Udid: %s\n", device_udid);
+			break;
+			case 'h':
+				print_help();
+				break;
+
+			default:
+				abort ();
+		}
+	}
+
+	return 0;
 }
